f10ebca6473343c4f545ff045c01d2ffcf7f1224
timofeya123 <timofeya123@inbox.ru>
2021-05-16 19:47:31 +0600

    comment_Shearing

--- src/com/AWT/ShearingEx.java
+++ src/com/AWT/ShearingEx.java
@@ -11,70 +11,70 @@ import javax.swing.JPanel;
 
 class SurfaceShearing extends JPanel {
 
-    private void doDrawing(Graphics g) {
+    private void doDrawing(Graphics g) {// графика
 
-        Graphics2D g2d = (Graphics2D) g.create();
+        Graphics2D g2d = (Graphics2D) g.create();// объявление класса Graphics2D
 
-        AffineTransform tx1 = new AffineTransform();
-        tx1.translate(50, 90);
+        AffineTransform tx1 = new AffineTransform();// наш эффект
+        tx1.translate(50, 90);// переместить
 
         g2d.setTransform(tx1);
-        g2d.setPaint(Color.green);
-        g2d.drawRect(0, 0, 160, 50);
+        g2d.setPaint(Color.green);// цвет
+        g2d.drawRect(0, 0, 160, 50);// закрасить прямоуголник
 
-        AffineTransform tx2 = new AffineTransform();
-        tx2.translate(50, 90);
+        AffineTransform tx2 = new AffineTransform();// наш эффект
+        tx2.translate(50, 90);// переместить
         tx2.shear(0, 1);
 
         g2d.setTransform(tx2);
-        g2d.setPaint(Color.blue);
+        g2d.setPaint(Color.blue);// цвет
 
-        g2d.draw(new Rectangle(0, 0, 80, 50));
+        g2d.draw(new Rectangle(0, 0, 80, 50));// закрасить прямоуголник
 
-        AffineTransform tx3 = new AffineTransform();
-        tx3.translate(130, 10);
+        AffineTransform tx3 = new AffineTransform();// наш эффект
+        tx3.translate(130, 10);// переместить
         tx3.shear(0, 1);
 
         g2d.setTransform(tx3);
-        g2d.setPaint(Color.red);
-        g2d.drawRect(0, 0, 80, 50);
+        g2d.setPaint(Color.red);// цвет
+        g2d.drawRect(0, 0, 80, 50);// закрасить прямоуголник
 
         g2d.dispose();
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {// компонент графики
 
         super.paintComponent(g);
-        doDrawing(g);
+        doDrawing(g);// вызов нашей графики
     }
 }
 
 public class ShearingEx extends JFrame {
 
-    public ShearingEx() {
-
+    public ShearingEx() { // конструктор
+        // инициализация
         initUI();
     }
 
     private void initUI() {
 
-        add(new SurfaceShearing());
+        add(new SurfaceShearing()); // добавление нашего объекта, на форму
 
         setTitle("Shearing");
-        setSize(330, 270);
+        setSize(330, 270);// размеры
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// выход
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// главный класс, начало кода
 
-        EventQueue.invokeLater(new Runnable() {
+        EventQueue.invokeLater(new Runnable() { // поток
             @Override
-            public void run() {
+            public void run() {// запуск
 
-                ShearingEx ex = new ShearingEx();
-                ex.setVisible(true);
+                ShearingEx ex = new ShearingEx();// создание нашего класса
+                ex.setVisible(true);// видимость
             }
         });
     }
