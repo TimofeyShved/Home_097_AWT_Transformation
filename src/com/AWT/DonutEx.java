d7d5f37c3b08d0362c539ff0a65bcf21b8e2ab43
timofeya123 <timofeya123@inbox.ru>
2021-05-16 19:54:50 +0600

    comment_Donut

--- src/com/AWT/DonutEx.java
+++ src/com/AWT/DonutEx.java
@@ -14,67 +14,67 @@ import javax.swing.JPanel;
 
 class SurfaceDonut extends JPanel {
 
-    private void doDrawing(Graphics g) {
+    private void doDrawing(Graphics g) {// графика
 
-        Graphics2D g2d = (Graphics2D) g.create();
+        Graphics2D g2d = (Graphics2D) g.create();// объявление класса Graphics2D
 
         RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+                RenderingHints.VALUE_ANTIALIAS_ON); // установка для прорисовки
 
         rh.put(RenderingHints.KEY_RENDERING,
                 RenderingHints.VALUE_RENDER_QUALITY);
 
-        g2d.setRenderingHints(rh);
+        g2d.setRenderingHints(rh); // установка рендоринга в графику
 
-        Dimension size = getSize();
+        Dimension size = getSize(); // размеры
         double w = size.getWidth();
         double h = size.getHeight();
 
-        Ellipse2D e = new Ellipse2D.Double(0, 0, 80, 130);
-        g2d.setStroke(new BasicStroke(1));
-        g2d.setPaint(Color.gray);
+        Ellipse2D e = new Ellipse2D.Double(0, 0, 80, 130); // овал
+        g2d.setStroke(new BasicStroke(1)); // толщина
+        g2d.setPaint(Color.gray); // цвет
 
-        for (double deg = 0; deg < 360; deg += 5) {
+        for (double deg = 0; deg < 360; deg += 5) { // цикл
             AffineTransform at =
-                    AffineTransform.getTranslateInstance(w / 2, h / 2);
-            at.rotate(Math.toRadians(deg));
-            g2d.draw(at.createTransformedShape(e));
+                    AffineTransform.getTranslateInstance(w / 2, h / 2); // наш эффект
+            at.rotate(Math.toRadians(deg)); // разварот
+            g2d.draw(at.createTransformedShape(e)); // прорисовка
         }
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {// компонент графики
 
         super.paintComponent(g);
-        doDrawing(g);
+        doDrawing(g);// вызов нашей графики
     }
 }
 
 public class DonutEx extends JFrame {
 
-    public DonutEx() {
-
+    public DonutEx() { // конструктор
+        // инициализация
         initUI();
     }
 
     private void initUI() {
 
-        add(new SurfaceDonut());
+        add(new SurfaceDonut());// добавление нашего объекта, на форму
 
         setTitle("Donut");
-        setSize(370, 320);
+        setSize(370, 320);// размеры
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// выход
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// главный класс, начало кода
 
-        EventQueue.invokeLater(new Runnable() {
+        EventQueue.invokeLater(new Runnable() { // поток
             @Override
-            public void run() {
+            public void run() {// запуск
 
-                DonutEx ex = new DonutEx();
-                ex.setVisible(true);
+                DonutEx ex = new DonutEx();// создание нашего класса
+                ex.setVisible(true);// видимость
             }
         });
     }
